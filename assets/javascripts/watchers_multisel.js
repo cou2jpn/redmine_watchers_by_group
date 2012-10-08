b886af043a09edd9644a7d5727da5b7f17868f0b
wookayin <wookayin@gmail.com>
2012-10-08 12:42:15 +0900

    fix javascript not to use global variables

--- assets/javascripts/watchers_multisel.js
+++ assets/javascripts/watchers_multisel.js
@@ -1,39 +1,45 @@
 function checkAll(name)
 {
-    boxes=document.getElementsByName(name)
-    for (i = 0; i < boxes.length; i++)
-	boxes[i].checked = true ;
+    boxes=document.getElementsByName(name);
+    for (var i = 0; i < boxes.length; i++)
+        boxes[i].checked = true ;
     return false;
 }
 
 function uncheckAll(name)
 {
-    boxes=document.getElementsByName(name)
-    for (i = 0; i < boxes.length; i++)
-	boxes[i].checked = false ;
+    boxes=document.getElementsByName(name);
+    for (var i = 0; i < boxes.length; i++)
+        boxes[i].checked = false ;
     return false;
 }
 
 function checkSome(name)
 {
-    var selected_values=document.getElementById('watcher_multiple_group').options[document.getElementById('watcher_multiple_group').selectedIndex].value.split(',');
-    boxes=document.getElementsByName(name)
-    for (i = 0; i < boxes.length; i++){
-        for (j = 0; j < selected_values.length; j++){
-            if(boxes[i].value == selected_values[j]) boxes[i].checked = true ;
+    var selected_values = document.getElementById('watcher_multiple_group').options[document.getElementById('watcher_multiple_group').selectedIndex].value.split(',');
+    boxes=document.getElementsByName(name);
+    for (var i = 0; i < boxes.length; i++){
+        for (var j = 0; j < selected_values.length; j++){
+            if(boxes[i].value == selected_values[j]) {
+                boxes[i].checked = true ;
+                break;
+            }
         }
-        }
+    }
     return false;
 }
 
 function uncheckSome(name)
 {
     var selected_values=document.getElementById('watcher_multiple_group').options[document.getElementById('watcher_multiple_group').selectedIndex].value.split(',');
-    boxes=document.getElementsByName(name)
-    for (i = 0; i < boxes.length; i++){
-        for (j = 0; j < selected_values.length; j++){
-            if(boxes[i].value == selected_values[j]) boxes[i].checked = false ;
+    boxes=document.getElementsByName(name);
+    for (var i = 0; i < boxes.length; i++){
+        for (var j = 0; j < selected_values.length; j++){
+            if(boxes[i].value == selected_values[j]) {
+                boxes[i].checked = false ;
+                break;
+            }
         }
-        }
+    }
     return false;
 }
