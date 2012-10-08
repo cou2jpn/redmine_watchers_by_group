68122b87f3ded54ca1c5cb0722ce441cf678b67d
wookayin <wookayin@gmail.com>
2012-10-08 14:55:30 +0900

    bump to version 0.1.0; fix readme and plugin descriptions

---
+++ README.md
@@ -0,0 +1,21 @@
+Redmine Select Watchers By Group Plugin
+=======================================
+
+Allows issue creator to select multiple watchers by group
+
+## Install
+
+(On directory REDMINE_HOME/vendor/plugins for Redmine 1.x or REDMINE_HOME/plugins for Redmine 2.0)
+
+```bash
+git clone https://github.com/wookayin/redmine_watchers_by_group
+```
+
+then just restart redmine server.
+
+## Version Compatibility
+
+This plugin is forked from the original one (from alivila) and maintained to support latest redmine versions.
+
+Compatible with Redmine 2.0.x and 2.1.x.
+
--- init.rb
+++ init.rb
@@ -3,9 +3,9 @@ require 'redmine'
 require_dependency 'hooks/hooks'
 Redmine::Plugin.register :redmine_watchers_by_group do
   name 'Watcher Sellection by Group'
-  author 'Milan Stastny of ALVILA SYSTEMS'
-  description 'Allows issue creator to select multiple watchers by group'
-  version '0.0.1'
-  author_url 'http://www.alvila.com'
+  author 'Jongwook Choi, and Milan Stastny of ALVILA SYSTEMS'
+  description 'Allows issue creator to select multiple watchers by group (forked to support latest redmine)'
+  version '0.1.0'
+  author_url 'https://github.com/wookayin/redmine_watchers_by_group'
 end
 
