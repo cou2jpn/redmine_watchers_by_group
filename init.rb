77c65b0182378f7e6c889787f87f82a383e3951b
wookayin <wookayin@gmail.com>
2013-01-19 09:10:30 +0900

    Bump version to 0.2.0 (the start of a new version)

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_watchers_by_group do
   name 'Watcher Sellection by Group'
   author 'Jongwook Choi, and Milan Stastny of ALVILA SYSTEMS'
   description 'Allows issue creator to select multiple watchers by group (forked to support latest redmine)'
-  version '0.1.0'
+  version '0.2.0'
   author_url 'https://github.com/wookayin/redmine_watchers_by_group'
 end
 
