8a0ec564e3615d7d090fd930c3d2c28f78526ee7
cou2jpn <anemone.reef@gmail.com>
2015-04-21 16:01:19 +0900

    Support Redmine3.0.

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_watchers_by_group do
   name 'Watcher Sellection by Group'
   author 'Jongwook Choi, and Milan Stastny of ALVILA SYSTEMS, and forked by cou2jpn'
   description 'Allows issue creator to select multiple watchers by group and roles.'
-  version '0.3.0'
+  version '0.3.1'
   author_url 'https://github.com/cou2jpn/redmine_watchers_by_group'
 end
 
